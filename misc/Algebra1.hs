f7e02faeb96cce6117ed2ec5024c5d60242d1f03
Tobias Schoofs <tobias.schoofs@gmx.net>
2016-02-14 22:28:02 +0000

    algebra1

--- misc/Algebra1.hs
+++ misc/Algebra1.hs
@@ -1,13 +1,15 @@
 module Algebra1
 where
 
-  import Data.List (findIndex,intercalate)
+  import Data.List (findIndex,intercalate,sort)
   import Data.Char (isDigit)
   import Control.Monad.Trans.Either
 
+  import Debug.Trace (trace)
+
   data Var = Var Char
            | Num Int
-    deriving (Eq,Show)
+    deriving (Eq,Ord,Show)
 
   mkVar :: String -> Either String Var
   mkVar x | numerical x   = Right (Num $ read x)
@@ -38,7 +40,7 @@ where
            | Add [String]
            | Mul [String]
            -- Div,Root
-    deriving (Eq,Show)
+    deriving (Eq,Ord,Show)
 
   eval :: String -> String
   eval s = case evalS s of
@@ -52,6 +54,9 @@ where
 
   evalS :: String -> Either String Exp
   evalS s = parse s >>= evalExp
+
+  simplifyS :: String -> Either String Exp
+  simplifyS s = parse s >>= simplify
 
   evalDistS :: String -> Either String Exp
   evalDistS s = parse s >>= distExp
@@ -132,6 +137,11 @@ where
       Exp v1 -> case x2 of
                   Exp v2  -> add x1 x2
                   x       -> Right (Add [pretty x1,pretty x2])
+      Mul m1 -> case x2 of
+                  Mul m2 -> if equal (Mul m1) (Mul m2)
+                              then Right (Mul (show 2:m1))
+                              else Right (Add [pretty x1,pretty x2])
+                  _      -> Right (Add [pretty x1, pretty x2])
       _      -> Right (Add [pretty x1,pretty x2])
 
   mul :: Exp -> Exp -> Either String Exp
@@ -142,24 +152,86 @@ where
     case x1 of 
       Exp v1 -> case x2 of
                   Exp v2 -> mul x1 x2
-                  Add _  -> Right (Mul [pretty (Exp v1), "(" ++ 
-                                        pretty x2 ++ ")"])
+                  Add _  -> distS [pretty x1, "(" ++
+                                   pretty x2 ++ ")"]
                   _      -> Right (Mul [pretty x1,pretty x2])
       Add _  -> case x2 of
-                  Add _ -> Right (Mul ["(" ++ pretty x1 ++ ")", 
-                                       "(" ++ pretty x2 ++ ")"])
-                  _     -> Right (Mul ["(" ++ pretty x1 ++ ")", 
-                                              pretty x2])
+                  Add _ -> distS [pretty x1, "(" ++
+                                  pretty x2 ++ ")"]
+                  _     -> distS ["(" ++ pretty x1 ++ ")",
+                                         pretty x2]
       _      -> case x2 of 
-                  Add _ -> Right (Mul [pretty x1, "(" ++ 
-                                       pretty x2 ++ ")"])
+                  Add _ -> distS [pretty x1, "(" ++
+                                  pretty x2 ++ ")"]
                   _     -> Right (Mul [pretty x1,pretty x2])
+
+  simplify :: Exp -> Either String Exp
+  simplify (Add []) = Left "empty add"
+  simplify (Add xs) = do
+    xs' <- simplifyArgs xs
+    Right (Add xs')
+
+  simplifyArgs :: [String] -> Either String [String]
+  simplifyArgs []  = Right []
+  simplifyArgs [s] = Right [s]
+  simplifyArgs (s1:s2:ss) = do
+    x1 <- evalS s1
+    x2 <- evalS s2
+    if equal x1 x2 then simplifyArgs (pretty (Mul [show 2,s1]):ss)
+                   else do r <- simplifyArgs (s2:ss)
+                           Right (s1:r)
+
+  equal :: Exp -> Exp -> Bool
+  equal (Exp v1) (Exp v2) = v1 == v2
+  equal (Add a1) (Add a2) = equalN a1 a2
+  equal (Mul a1) (Mul a2) = let b1 = map sort a1 
+                                b2 = map sort a2
+                                c1 = sort b1
+                                c2 = sort b2
+                             in c1 == c2
+  equal _ _ = False
+
+  equalN :: [String] -> [String] -> Bool
+  equalN xs ys = go (sort xs) (sort ys)
+    where go [] [] = True
+          go [] _  = False
+          go _  [] = False
+          go (a:as) (b:bs) = 
+            case evalS a of 
+              Left _ -> False
+              Right x1 -> case evalS b of
+                            Left _ -> False
+                            Right x2 -> if equal x1 x2 then go as bs
+                                                       else False
+            
 
 
   pretty :: Exp -> String
   pretty (Exp v)  = prettyv v
-  pretty (Add as) = intercalate "+" as 
+  pretty (Add as) = intercalate "+" as
   pretty (Mul as) = concat as
+
+  type Stack = [String]
+
+  push :: Stack -> String -> Stack
+  push s e = e:s
+
+  pop :: Stack -> Either String (String,Stack)
+  pop [] = Left "Stack is empty"
+  pop s  = Right (head s, tail s)
+
+  toList :: Stack -> [String]
+  toList = reverse
+
+  addStk :: Stack -> String -> Either String Stack
+  addStk s y = do
+    (x,s') <- pop s
+    Right (push s' (x++"+"++y))
+
+  mulStk :: Stack -> String -> Either String Stack
+  mulStk s y = do
+    (x,s') <- pop s
+    Right (push s' (x++y))
 
   parse :: String -> Either String Exp
   parse s = getexp s "" []
@@ -169,17 +241,23 @@ where
   getexp "" "" s   = Right (Mul s)
   getexp "" t  s   = Right (Mul (s++[t]))
   getexp ('(':xs) t s = do
-    x <- close xs 
+    x <- close ('(' : xs) 
     case x of
       ("",_) -> Left "empty parentheses"
-      (a,"") -> if null t then Right (Mul (s++[a])) 
-                          else Right (Mul (s++[t,a]))
+      (a,"") -> if null s && null t 
+                then let a' = takeWhile (/= ')') $ drop 1 a
+                      in getexp a' "" []
+                else if null t then Right (Mul (s++[a])) 
+                               else Right (Mul (s++[t,a]))
       (a,zs) -> if null t then getexp zs "" (s++[a])
-                          else getexp zs "" (s++[t,a])
-  getexp ('+':xs) t s = Right (Add (s++[t,xs]))
+                          else if null s then getexp zs "" [t++a]
+                                         else getexp zs "" (s++[t++a])
+  getexp ('+':xs) t s = if null t then Right (Add (s++[xs]))
+                                  else if null s then Right (Add [t,xs])
+                                       else Right (Add (init s++[last s++t]++[xs]))
   getexp (x:xs)   t s = getexp xs (t++[x]) s
 
   close :: String -> Either String (String,String)
   close s = case findIndex (==')') s of
               Nothing -> Left "parentheses not closed"
-              Just i  -> Right (take i s,drop (i+1) s)
+              Just i  -> Right (take (i+1) s,drop (i+1) s)
